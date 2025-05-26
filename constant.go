1a29d26a6febdc7f88438449ad6e8f72c4a5fafb
eben <eben.katilik@sinarmassekuritas.co.id>
2025-05-26 13:50:49 +0700

    invalid file extension

--- constant.go
+++ constant.go
@@ -17,23 +17,24 @@ const (
 	SCodeLogout    = 11
 
 	// Fail response code
-	FCodeDataNotFound        = 1
-	FCodeDb                  = 2
-	FCodeBadRequest          = 3
-	FCodeUriNotFound         = 4
-	FCodeSelfRefer           = 5
-	FCodeMethodNotAllowed    = 6
-	FCodeDuplicateEntry      = 7
-	FCodeMaximumLimit        = 8
-	FCodeUnauthorized        = 9
-	FCodeOneDevicePolicy     = 10
-	FCodeInvalidLogin        = 11
-	FCodeOTP                 = 12
-	FCodeGeneral             = 13
-	FCodeInternalServerError = 14
-	FCodeIncompleteData      = 15
-	FCodeTokenInvalid        = 16
-	FCodeTokenExpired        = 17
+	FCodeDataNotFound         = 1
+	FCodeDb                   = 2
+	FCodeBadRequest           = 3
+	FCodeUriNotFound          = 4
+	FCodeSelfRefer            = 5
+	FCodeMethodNotAllowed     = 6
+	FCodeDuplicateEntry       = 7
+	FCodeMaximumLimit         = 8
+	FCodeUnauthorized         = 9
+	FCodeOneDevicePolicy      = 10
+	FCodeInvalidLogin         = 11
+	FCodeOTP                  = 12
+	FCodeGeneral              = 13
+	FCodeInternalServerError  = 14
+	FCodeIncompleteData       = 15
+	FCodeTokenInvalid         = 16
+	FCodeTokenExpired         = 17
+	FCodeInvalidFileExtension = 18
 )
 
 // Success message
@@ -53,23 +54,24 @@ var sCodeText = map[int]string{
 
 // Fail message
 var fCodeText = map[int]string{
-	FCodeDataNotFound:        "data not found",
-	FCodeDb:                  "data source error",
-	FCodeBadRequest:          "bad request",
-	FCodeUriNotFound:         "uri not found",
-	FCodeSelfRefer:           "self-refer is not allowed",
-	FCodeMethodNotAllowed:    "method not allowed",
-	FCodeDuplicateEntry:      "duplicate entry",
-	FCodeMaximumLimit:        "maximum limit reached",
-	FCodeUnauthorized:        "unauthorized",
-	FCodeOneDevicePolicy:     "multiple accounts in one device is not allowed",
-	FCodeInvalidLogin:        "invalid login",
-	FCodeOTP:                 "otp code: invalid / expired / reach max attempt",
-	FCodeGeneral:             "general error",
-	FCodeInternalServerError: "internal server error",
-	FCodeIncompleteData:      "incomplete data",
-	FCodeTokenInvalid:        "invalid token",
-	FCodeTokenExpired:        "expired token",
+	FCodeDataNotFound:         "data not found",
+	FCodeDb:                   "data source error",
+	FCodeBadRequest:           "bad request",
+	FCodeUriNotFound:          "uri not found",
+	FCodeSelfRefer:            "self-refer is not allowed",
+	FCodeMethodNotAllowed:     "method not allowed",
+	FCodeDuplicateEntry:       "duplicate entry",
+	FCodeMaximumLimit:         "maximum limit reached",
+	FCodeUnauthorized:         "unauthorized",
+	FCodeOneDevicePolicy:      "multiple accounts in one device is not allowed",
+	FCodeInvalidLogin:         "invalid login",
+	FCodeOTP:                  "otp code: invalid / expired / reach max attempt",
+	FCodeGeneral:              "general error",
+	FCodeInternalServerError:  "internal server error",
+	FCodeIncompleteData:       "incomplete data",
+	FCodeTokenInvalid:         "invalid token",
+	FCodeTokenExpired:         "expired token",
+	FCodeInvalidFileExtension: "invalid file extension",
 }
 
 // Success http status
@@ -89,21 +91,22 @@ var sHttpStatus = map[int]int{
 
 // Fail http status
 var fHttpStatus = map[int]int{
-	FCodeDataNotFound:        http.StatusOK,
-	FCodeDb:                  http.StatusInternalServerError,
-	FCodeBadRequest:          http.StatusBadRequest,
-	FCodeUriNotFound:         http.StatusNotFound,
-	FCodeSelfRefer:           http.StatusOK,
-	FCodeMethodNotAllowed:    http.StatusMethodNotAllowed,
-	FCodeDuplicateEntry:      http.StatusOK,
-	FCodeMaximumLimit:        http.StatusOK,
-	FCodeUnauthorized:        http.StatusUnauthorized,
-	FCodeOneDevicePolicy:     http.StatusOK,
-	FCodeInvalidLogin:        http.StatusOK,
-	FCodeOTP:                 http.StatusOK,
-	FCodeGeneral:             http.StatusOK,
-	FCodeInternalServerError: http.StatusInternalServerError,
-	FCodeIncompleteData:      http.StatusOK,
-	FCodeTokenInvalid:        http.StatusOK,
-	FCodeTokenExpired:        http.StatusOK,
+	FCodeDataNotFound:         http.StatusOK,
+	FCodeDb:                   http.StatusInternalServerError,
+	FCodeBadRequest:           http.StatusBadRequest,
+	FCodeUriNotFound:          http.StatusNotFound,
+	FCodeSelfRefer:            http.StatusOK,
+	FCodeMethodNotAllowed:     http.StatusMethodNotAllowed,
+	FCodeDuplicateEntry:       http.StatusOK,
+	FCodeMaximumLimit:         http.StatusOK,
+	FCodeUnauthorized:         http.StatusUnauthorized,
+	FCodeOneDevicePolicy:      http.StatusOK,
+	FCodeInvalidLogin:         http.StatusOK,
+	FCodeOTP:                  http.StatusOK,
+	FCodeGeneral:              http.StatusOK,
+	FCodeInternalServerError:  http.StatusInternalServerError,
+	FCodeIncompleteData:       http.StatusOK,
+	FCodeTokenInvalid:         http.StatusOK,
+	FCodeTokenExpired:         http.StatusOK,
+	FCodeInvalidFileExtension: http.StatusUnprocessableEntity,
 }
